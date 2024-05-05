2f0689b4ae5c06a1d8d69c40a6be7d02fa8cc9f1
ReySnts <reynaldy.sentosa@binus.ac.id>
2024-05-05 20:51:39 +0700

    Add Some Dialogues #1

---
+++ VR-GPPI/Assets/Scripts/ScriptableObjects/Dialogue.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Dialogue", menuName = "ScriptableObjects/Dialogue")]
+public class Dialogue : ScriptableObject
+{
+    [Space(height: 10f)] public Vector3 spawnPosition;
+
+    [Space(height: 10f), TextArea(minLines: 1, maxLines: 20)] public string text;
+}
